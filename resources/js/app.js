29fa928176c0d27254fd5639e5fb9e011aefc233
rooberthh <roberth.evaldsson@hotmail.com>
2019-11-25 21:00:08 +0100

    file upload component

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,6 +19,7 @@ window.Vue = require('vue');
 
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('dropdown-menu', require('./components/Dropdown.vue').default);
+Vue.component('file-upload', require('./components/FileUpload.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/components.blade.php
+++ resources/views/components.blade.php
@@ -14,9 +14,9 @@
 <body>
     <div id="app" class="flex flex-col items-center p-8">
         <h1 class="text-xl font-bold mb-8">Components</h1>
-        <div>
-            <h1 class="text-l">Context-menu</h1>
-            <div class="bg-gray-400 w-32 h-32 flex items-center justify-center">
+        <section>
+            <h2 class="text-l">Context-menu</h2>
+            <div class="bg-gray-400 w-64 h-32 flex items-center justify-center">
                 <dropdown-menu>
                     <template v-slot:trigger>
                         <button class="text-blue-500">...</button>
@@ -27,19 +27,15 @@
                     <li><a href="#" class="dropdown-item hover:bg-gray-200">Report</a></li>
                 </dropdown-menu>
             </div>
-            <hr>
-            <div class="bg-gray-400 w-32 h-32 flex items-center justify-center">
-                <dropdown-menu>
-                    <template v-slot:trigger>
-                        <button class="text-blue-500">...</button>
-                    </template>
+        </section>
+        <hr>
+        <section>
 
-                    <li><a href="#" class="dropdown-item hover:bg-gray-200">Edit</a></li>
-                    <li><a href="#" class="dropdown-item hover:bg-gray-200">Delete</a></li>
-                    <li><a href="#" class="dropdown-item hover:bg-gray-200">Report</a></li>
-                </dropdown-menu>
+            <h2 class="text-l">File Upload</h2>
+            <div class="bg-gray-400 w-64 h-32 flex items-center justify-center">
+                <file-upload name="image_path"></file-upload>
             </div>
-        </div>
+        </section>
     </div>
 
     <script src="{{asset('/js/app.js')}}" crossorigin="anonymous"></script>
